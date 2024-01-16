7ac325cb9a49dc5ed119ae23ee5a5b14f1232e93
eskovdmt <DmtEskov@gmail.com>
2024-01-16 12:45:17 +0300

    feat: add rubocop

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 Sentry.init do |config|
-  config.dsn = 'https://70d7e8944a1bbc220d4643c754997a4f@o4506157839286272.ingest.sentry.io/4506579877756928'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.dsn = ENV['SENTRY_KEY']
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |context|
+  config.traces_sampler = lambda do |_context|
     true
   end
 end
