557c44353e82e7bfb1a6cd5fb2fe113485d83a20
eskovdmt <DmtEskov@gmail.com>
2024-02-12 21:40:26 +0300

    feat: run_lint for web_hook test

--- app/controllers/api/webhooks_controller.rb
+++ app/controllers/api/webhooks_controller.rb
@@ -23,6 +23,7 @@ module Api
         client = Linter::LinterFactory.create_linter(repository.language)
         repo_path = Linter::RepositoryDownloader.download(repository.git_url)
         check_lint_result, error_lint_status = client.lint(repo_path)
+        commit_id = client.current_commit(repo_path)
         unless repository.checks.create(repo_path:, check_result: check_lint_result.to_json,
                                          status: error_lint_status, commit_id: commit_id)
           redirect_to root_path
