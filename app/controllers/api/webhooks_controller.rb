3e2f841519f7a4ddbc96c519d45cfa8fa909672a
eskovdmt <DmtEskov@gmail.com>
2024-03-06 10:44:41 +0300

    feat: change webhook logic

--- app/controllers/api/webhooks_controller.rb
+++ app/controllers/api/webhooks_controller.rb
@@ -17,18 +17,10 @@ module Api
 
     def run_lint(git_id)
       repository = Repository.find_by(git_id:)
-      puts repository
-      puts git_id
       if repository
-        client = Linter::LinterFactory.create_linter(repository.language)
-        repo_path = Linter::RepositoryDownloader.download(repository.git_url)
-        check_lint_result, error_lint_status = client.lint(repo_path)
-        commit_id = client.current_commit(repo_path)
-        unless repository.checks.create(repo_path:, check_result: check_lint_result.to_json,
-                                         status: error_lint_status, commit_id: commit_id)
-          redirect_to root_path
-
-        end
+        check = repository.checks.create!
+        RepositoryCheckJob.perform_later(repository.id, check.id)
+        redirect_to repository_path(@repository), notice: "Check has been scheduled"
       end
     end
 
