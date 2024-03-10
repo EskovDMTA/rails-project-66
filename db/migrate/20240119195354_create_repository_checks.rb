8bec30f8a79244c75f7a81623013f88cb92ecd7b
eskovdmt <DmtEskov@gmail.com>
2024-03-11 00:18:03 +0300

    feat: fix column_name

--- db/migrate/20240119195354_create_repository_checks.rb
+++ db/migrate/20240119195354_create_repository_checks.rb
@@ -4,7 +4,7 @@ class CreateRepositoryChecks < ActiveRecord::Migration[7.1]
   def change
     create_table :repository_checks do |t|
       t.references :repository, null: false, foreign_key: true
-      t.boolean :status, default: false, null: false
+      t.boolean :passed, default: false, null: false
       t.string :aasm_state
       t.string :commit_id
       t.string :repo_path
