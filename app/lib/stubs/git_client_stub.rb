e44d55192f04fcaef9d779db2c8989f185db2a59
eskovdmt <DmtEskov@gmail.com>
2024-03-18 22:53:15 +0300

    fix: repository_params

--- app/lib/stubs/git_client_stub.rb
+++ app/lib/stubs/git_client_stub.rb
@@ -4,9 +4,9 @@ module Stubs
   class GitClientStub
     def initialize(*) end
 
-    def repository_params(git_hub_repo_id)
-      current_repo = user_repositories.filter { |repo| repo[:id] == 1296269 }.first
-      puts "current_repo"
+    def repository_params(_git_hub_repo_id)
+      current_repo = user_repositories.filter { |repo| repo[:id] == 1_296_269 }.first
+      puts 'current_repo'
       puts current_repo
       {
         name: current_repo[:name],
@@ -14,14 +14,15 @@ module Stubs
         language: current_repo[:language].downcase,
         git_url: current_repo[:clone_url],
         ssh_url: current_repo[:ssh_url],
-        github_id: git_hub_repo_id
+        github_id: 1_296_269
       }
     end
 
     def fetch_user_repositories_name_and_id
-      user_repositories.select { |repo| repo[:language] == 'javascript' || repo[:language] == 'ruby' }
-                       .sort_by { |repo| repo[:full_name] }
-                       .map { |repo| [repo[:full_name], repo[:id]] }
+      # user_repositories.select { |repo| repo[:language] == 'javascript' || repo[:language] == 'ruby' }
+      #                  .sort_by { |repo| repo[:full_name] }
+      #                  .map { |repo| [repo[:full_name], repo[:id]] }
+      ['octocat/Hello-World', 1_296_269]
     end
 
     def client
