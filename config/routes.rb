0f821ea177aa0bf37adbe2a15b51c5aac11f9f96
eskovdmt <DmtEskov@gmail.com>
2024-03-20 09:57:16 +0300

    fix: routes bag

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   namespace :api do
-    post 'checks'
+    post 'checks', to: 'checks#github'
   end
 
   # Defines the root path route ("/")
